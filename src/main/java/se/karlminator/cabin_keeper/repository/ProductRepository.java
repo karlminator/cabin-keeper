8929d873be46df575d3ccbe9f7f41075557283ac
karlminator <kf.holmsten@gmail.com>
2025-05-12 14:30:49 +0200

    added custom queries to fix lazy loading issue with related entities

--- src/main/java/se/karlminator/cabin_keeper/repository/ProductRepository.java
+++ src/main/java/se/karlminator/cabin_keeper/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package se.karlminator.cabin_keeper.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.karlminator.cabin_keeper.model.Product;
 import se.karlminator.cabin_keeper.model.Room;
@@ -12,9 +14,14 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    @Query("SELECT DISTINCT p FROM Product p LEFT JOIN FETCH p.room LEFT JOIN FETCH p.categories")
+    List<Product> findAllWithRoomAndCategories();
+
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.room LEFT JOIN FETCH p.categories WHERE p.id = :id")
+    Optional<Product> findByIdWithRoomAndCategories(@Param("id") Integer id);
+
     List<Product> findByNameContainingIgnoreCase(String name);
     List<Product> findByName(String name);
-    List<Product> findByRoom(Room room);
     List<Product> findByRoomId(Integer roomId);
     List<Product> findByCategoriesId(Integer categoryId);
 }
